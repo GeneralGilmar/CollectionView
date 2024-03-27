51012b315cdda407bae106f2d842456abe51864b
GeneralGilmar <gaspar.general1987@gmail.com>
2024-03-27 11:24:42 +0000

    Collection view Training

---
+++ CollectionViewDemo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CollectionViewDemo.MVVM.Views;
+
+
+namespace CollectionViewDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ProductsView();
+        }
+    }
+}
---
+++ CollectionViewDemo/Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using CollectionViewDemo.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewDemo.Selectors
+{
+    class ProductDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var Product =
+                item as Product;
+            if (!Product.HasOffer)
+            {
+                Application.Current.Resources.TryGetValue("ProductStyle", out var productStyle);
+                return productStyle as DataTemplate;
+
+            }else
+            {
+                Application.Current.Resources.TryGetValue("OfferStyle", out var OfferStyle);
+                return OfferStyle as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+       
+    }
+}
